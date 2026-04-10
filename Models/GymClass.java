0ec64ecbb6759cedf3e46845a2a123c893ddf61c
MattReid1982 <mattwcreid@hotmail.com>
2026-04-10 14:20:51 -0230

    Created the GymClass class, set up the getters and setters and added a add member or remove member option.

--- Models/GymClass.java
+++ Models/GymClass.java
@@ -1,5 +1,62 @@
 package Models;
 
 public class GymClass {
+    private int classId;
+    private String className;
+    private String classSchedule;
+    private int capacity;
+    private int enrolled;
+
+    public GymClass(int classId, String className, String classSchedule, int capacity) {
+        this.classId = classId;
+        this.className = className;
+        this.classSchedule = classSchedule;
+        this.capacity = capacity;
+        this.enrolled = 0; // Initially, no one is enrolled
+    }
+
+    // Getters 
+    public int getClassId() {
+        return classId;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public String getClassSchedule() {
+        return classSchedule;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getEnrolled() {
+        return enrolled;
+    }
+
+    public boolean addMember() {
+        if (enrolled < capacity) {
+            enrolled++;
+            return true; // Successfully added a member
+        } else {
+            return false; // Class is full
+        }
+    }
+
+    public boolean removeMember() {
+        if (enrolled > 0) {
+            enrolled--;
+            return true; // Successfully removed a member
+        } else {
+            return false; // No members to remove
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Class: " + className + " | Schedule: " + classSchedule + " | Capacity: " + capacity + " | Enrolled: " + enrolled;
     
+    }
 }
