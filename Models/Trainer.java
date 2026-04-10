5ab88fbb2db57425ed81de50f7b532fd39b4b232
MattReid1982 <mattwcreid@hotmail.com>
2026-04-10 14:03:03 -0230

    Set up trainer as an extension of user, added getters and setters.

--- Models/Trainer.java
+++ Models/Trainer.java
@@ -1,5 +1,34 @@
 package Models;
 
-public class Trainer {
-    
+// Trainer class extending User, representing a trainer in the GMS
+class Trainer extends User {
+    private String speciality;
+    private int yearsExperience;
+
+    public Trainer(int id, String name, String email, String speciality, int yearsExperience) {
+        super(id, name, email);
+        this.speciality = speciality;
+        this.yearsExperience = yearsExperience;
+    }
+// Getters
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public int getYearsExperience() {
+        return yearsExperience;
+    }
+// Setters
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public void setYearsExperience(int yearsExperience) {
+        this.yearsExperience = yearsExperience;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Speciality: " + speciality + ", Years of Experience: " + yearsExperience;
+    }
 }
